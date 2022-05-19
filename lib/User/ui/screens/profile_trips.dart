56344cc60a06e73f665360c11a9ec45810d1ec40
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-05-19 07:21:21 -0400

    initial commit

---
+++ lib/User/ui/screens/profile_trips.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:trips_app/User/ui/widgets/profile_background.dart';
+import 'package:trips_app/User/ui/widgets/profile_places_list.dart';
+
+import 'profile_header.dart';
+
+class ProfileTrips extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    /*return Container(
+      color: Colors.indigo,
+    );*/
+    return Stack(
+      children: <Widget>[
+        ProfileBackground(),
+        ListView(
+          children: <Widget>[ProfileHeader(), ProfilePlacesList()],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/platzi_trips_cupertino.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:trips_app/User/ui/screens/profile_trips.dart';
+
+import 'Place/ui/screens/home_trips.dart';
+import 'Place/ui/screens/search_trips.dart';
+
+class PlatziTripsCupertino extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      bottomNavigationBar: CupertinoTabScaffold(
+        backgroundColor: Colors.transparent,
+        tabBar: CupertinoTabBar(
+            backgroundColor: Colors.white.withAlpha(70),
+            activeColor: Colors.indigo,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search), label: "Search"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), label: "Person"),
+            ]),
+        tabBuilder: (BuildContext context, int index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(
+                builder: (BuildContext context) => HomeTrips(),
+              );
+            case 1:
+              return CupertinoTabView(
+                builder: (BuildContext context) => SearchTrips(),
+              );
+            case 2:
+              return CupertinoTabView(
+                builder: (BuildContext context) => ProfileTrips(),
+              );
+            default:
+              return CupertinoTabView(
+                builder: (BuildContext context) => HomeTrips(),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
